0a713b1d44984b4bef52bcb7e4ea1077c3a2a62c
unknown <12cmorrison12@gmail.com>
2022-09-21 17:03:16 -0600

    q3

--- app.js
+++ app.js
@@ -14,3 +14,6 @@ Remove the div with a class of footer */
 const container = document.getElementById('container');
 
 const containerQ = document.querySelector('#container');
+
+const classOne = document.querySelectorAll('.second');
+
