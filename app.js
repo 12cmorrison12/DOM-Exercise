717ae9d0a5cf27cc0f4720cace8a1f057a881a1d
unknown <12cmorrison12@gmail.com>
2022-09-21 18:07:00 -0600

    fixed answers

--- app.js
+++ app.js
@@ -8,41 +8,38 @@ const containerQ = document.querySelector('#container');
 const classOne = document.querySelectorAll('.second');
 
 // Select a list item with a class of third, but only the list item inside of the ol tag.
-const liThird = document.getElementsByTagName('ol');
-liThird.getElementsByClassName('third'); 
+const liThird = document.querySelector('ol .third'); 
 
 // Give the section with an id of container the text “Hello!”.
 container.innerText = "Hello!";
 
 // Add the class main to the div with a class of footer.
 const footer = document.getElementsByClassName('footer');
-footer.createElement('div');
+footer.classList.add('main');
 
 // Remove the class main on the div with a class of footer.
-footer.remove('main');
+footer.classList.remove('main');
 
-// Create a new li element.
-const uls = document.querySelector('ul');
-
+// Create a new li elements
 const newLi = document.createElement("li");
 
 // Give the li the text “four”.
 newLi.innerText = "four";
 
 // Append the li to the ul element.
-uls.append(newLi);
+const uls = document.querySelector('ul');
+uls.appendChild(newLi);
 
 // Loop over all of the lis inside the ol tag and give them a background color of “green”.
-const ols = document.querySelector('ol');
-const lis = ols.querySelectorAll('lis');
+const lisInOl = document.querySelector('ol li');
 
-for(let li of lis) {
-    li.style.color = "green";
+for(let li of lisInOl) {
+    li.style.backgroundColor = "green";
 }
 
 // Remove the div with a class of footer
 const toRemove = document.getElementsByClassName('footer');
-toRemove.remove('div');
+toRemove.remove();
 
 
 
